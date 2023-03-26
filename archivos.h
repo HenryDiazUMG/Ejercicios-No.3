fb20236b2fb957500b30439adbf486ebdbd8d56a
Henry Díaz <hdiazd3@miumg.edu.gt>
2023-03-25 23:30:28 -0600

    EjerciciosNo.3

---
+++ archivos.h
@@ -0,0 +1,16 @@
+
+#ifndef REPASOEJERCICIOS3_ARCHIVOS_H
+#define REPASOEJERCICIOS3_ARCHIVOS_H
+
+#endif //REPASOEJERCICIOS3_ARCHIVOS_H
+
+using namespace std;
+
+void edadmayorme();
+void nummayor();
+void parimpar();
+void factorial();
+void numerosimparesfor();
+void numerosimpareswhile();
+void diadelasemana();
+void ejerciciosanteriores();
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "archivos.h"
+
+using namespace std;
+
+int main() {
+    ejerciciosanteriores();
+}
